471a137aa057903f19710ca057fee3f9a461f115
SytheMusic <clawschaospsn@gmail.com>
2017-12-24 18:04:11 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,20 @@ client.on('message', message => {
     	message.reply('yay you did nothing');
   	}
 });
+ client.on('message', message => {  
+    if (message.content === '/join') {
+    
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
