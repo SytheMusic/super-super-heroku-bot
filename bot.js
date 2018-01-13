a6d2b5176c58c9155327add40dc061ff5350e1eb
SytheMusic <clawschaospsn@gmail.com>
2018-01-13 15:21:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,51 +5,10 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-bot.on('ready', () => {
-  bot.user.setGame('GAME HERE')
-})
-
 client.on('message', message => {
-    if (message.content === 'screw you') {
-    	message.reply('screw you too! ');
+    if (message.content === '!submit') {
+    	message.reply('**Please put a invite link and some info about your website or server.** ');
   	}
-});
-
-client.on('message', message => {
-    if (message.content === 'nothing') {
-    	message.reply('yay you did nothing');
-  	}
-});
- client.on('message', message => {  
-    if (message.content === '/join') {
-       message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
-    }
-  }
 });
 
 
